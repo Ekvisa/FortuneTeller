0e2d7cb12405173cdb391b4cce56cb75798717f2
Ekvisa <katishh@gmail.com>
2026-06-13 21:02:16 +0400

    correct layout

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,20 @@ import close from "../src/assets/buttons/close.svg";
 import heart from "../src/assets/buttons/heart.svg";
 
 type Card = {
-  id: string;
+  // id: string;
   advice: string;
   image: string;
-  favorite: boolean;
+  // favorite: boolean;
 };
 
 type dataStatus = "idle" | "loading" | "ready" | "error";
 
 function App() {
-  const [status, setStatus] = useState<dataStatus>("idle");
+  const [status, setStatus] = useState<dataStatus>("idle"); //статус данных
   const [error, setError] = useState<Error | null>(null);
-
   const [currentCard, setCurrentCard] = useState<Card | null>(null);
-  const [history, setHistory] = useState<Card[]>([]);
+  const [history, setHistory] = useState<Card[]>([]); //История и
+  const [favorites, setFavorites] = useState<Card[]>([]); //Избранное, в каждом будем хранить максимум 10 карт
 
   async function getData() {
     try {
@@ -55,22 +55,29 @@ function App() {
     setStatus("ready");
   }
 
+  //При закрытии карты добавим её в Историю:
   function closeClick() {
     if (!currentCard) return;
-    setHistory((prev) => [currentCard, ...prev]);
+    setHistory((prev) => [currentCard, ...prev.slice(0, 9)]);
+
     setCurrentCard(null);
     setStatus("idle");
   }
 
+  //При нажатии лайка удалим (по айди) или добавим её в Избранное:
   function favoriteClick() {
     if (!currentCard) return;
-    setCurrentCard({ ...currentCard, favorite: !currentCard.favorite });
+    if (favorites.includes(currentCard)) {
+      setFavorites((prev) => prev.filter((card: Card) => card === currentCard));
+    } else {
+      setFavorites((prev) => [currentCard, ...prev.slice(0, 9)]);
+    }
   }
 
   async function showCardOfTheDay() {
-    const card = await getData();
-    if (!card) return;
-    setCurrentCard(card);
+    const cardOfTheDay = await getData();
+    if (!cardOfTheDay) return;
+    setCurrentCard(cardOfTheDay);
     setStatus("ready");
   }
 
@@ -78,27 +85,48 @@ function App() {
     console.log(history);
   }
 
+  function showFavorites() {
+    console.log(favorites);
+  }
+
   return (
     <div className="app">
-      <button onClick={crystalClick} disabled={status === "loading"}>
-        {status === "loading" ? "Гадаю..." : "✨ Погадать ✨"}
-      </button>
-      <div className="main">
-        {
-          <div
-            onClick={crystalClick}
-            className={`crystal abs ${status === "loading" ? "shiny" : ""} ${status === "idle" ? "sleep" : ""}`}
-          >
-            🔮
-          </div>
-        }
+      <div className="actions">
+        <div
+          onClick={crystalClick}
+          className={`crystal ${status === "loading" ? "shiny" : ""} ${status === "idle" ? "sleep" : ""}`}
+        >
+          🔮
+        </div>
+
+        <p onClick={showCardOfTheDay}>- карта дня -</p>
+        <p onClick={showHistory}>- история -</p>
+        {history.map((card, index) => (
+          <ul>
+            <li key={index} onClick={() => setCurrentCard(card)}>
+              <span>🎴</span> {card.advice.slice(0, 20)}...
+            </li>
+          </ul>
+        ))}
+
+        <p onClick={showFavorites}>- избранное -</p>
+        {favorites.map((card, index) => (
+          <ul>
+            <li key={index} onClick={() => setCurrentCard(card)}>
+              <span>🎴</span> {card.advice.slice(0, 20)}...
+            </li>
+          </ul>
+        ))}
+      </div>
+
+      <div className="board">
         {error && (
           <div className="error abs">
             Ошибка 😢 <br /> Попробуйте включить VPN 🪄
           </div>
         )}
 
-        <div className={`abs ${status === "ready" ? "shown" : "invisible"}`}>
+        <div className={`abs ${status === "ready" ? "specified" : "empty"}`}>
           {currentCard && (
             <div className="card">
               <div className="data">
@@ -118,13 +146,6 @@ function App() {
             </div>
           )}
         </div>
-      </div>
-
-      <div className="bottom">
-        <p onClick={showCardOfTheDay}>- карта дня -</p>
-        <p onClick={showHistory}>- история -</p>
-
-        <p>- избранное -</p>
       </div>
     </div>
   );
